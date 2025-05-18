6ac362829875437e547b81142c57eb933496dd6f
sameh-amr <samehamrsw@gmail.com>
2025-05-18 16:45:43 +0300

    finalized the sign up

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import StatsSection from "./components/StatsSection";
@@ -5,20 +6,33 @@ import FaqSection from "./components/FaqSection";
 import ShowcaseSection from "./components/ShowcaseSection";
 import PricingSection from "./components/PricingSection";
 import Footer from "./components/Footer";
+import SignUp from "./components/SignUp";
 import React from "react";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Hero />
-      <StatsSection />
-      <FaqSection />
-      <ShowcaseSection />
-      <PricingSection />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Navbar />
+                <Hero />
+                <StatsSection />
+                <FaqSection />
+                <ShowcaseSection />
+                <PricingSection />
+                <Footer />
+              </>
+            }
+          />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
